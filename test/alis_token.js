af4080619d4acc8618625ce362ae5d01bfa993fa
sot528 <sot528@gmail.com>
2017-07-23 02:07:01 +0900

    add test of offered amount.

--- test/alis_token.js
+++ test/alis_token.js
@@ -2,14 +2,24 @@
 const AlisToken = artifacts.require('AlisToken.sol');
 
 contract('AlisToken', (accounts) => {
-  // noinspection Annotator
-  it('should put 500,000,000 AlisToken in the first account', () => AlisToken.deployed().then(
-    instance => (
-      instance.balanceOf.call(accounts[0])
-    ),
-  ).then(
-    (balance) => {
-      assert.equal(balance.valueOf() / (10 ** 18), 500000000, `wrong token amount: ${balance.valueOf()}`);
-    }),
-  );
+  let alisToken; // Deployed AlisToken.
+
+  describe('CONTRACT DEPLOYMENT', () => {
+    it('should put 500,000,000 AlisToken in the first account', () => AlisToken.deployed().then(
+      (instance) => {
+        alisToken = instance;
+        return instance.balanceOf.call(accounts[0]);
+      },
+    ).then(
+      (balance) => {
+        assert.equal(balance.valueOf() / (10 ** 18), 500000000, `wrong token amount: ${balance.valueOf()}`);
+      }),
+    );
+
+    it('should put 250,000,000 amount in the offeredAmount property.', () => alisToken.offeredAmount().then(
+      amount => (
+        assert.equal(amount.valueOf() / (10 ** 18), 250000000)
+      )),
+    );
+  });
 });
