13770f880b44929425ba17c91c49f65441480575
sot528 <sot528@gmail.com>
2017-07-23 01:05:02 +0900

    add offered amount of token.

--- contracts/AlisToken.sol
+++ contracts/AlisToken.sol
@@ -5,14 +5,18 @@ import "tokens/HumanStandardToken.sol";
 
 
 contract AlisToken is HumanStandardToken {
+
+  uint256 public offeredAmount; // Amount of AlisTokens offered to the public.
+
   function AlisToken(
   uint256 _initialAmount,
   string _tokenName,
   uint8 _decimalUnits,
-  string _tokenSymbol
+  string _tokenSymbol,
+  uint256 _offeredAmount
   )
   HumanStandardToken(_initialAmount, _tokenName, _decimalUnits, _tokenSymbol)
   {
-    // TODO
+    offeredAmount = _offeredAmount;
   }
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,5 +4,9 @@ const AlisToken = artifacts.require('AlisToken.sol');
 const initParams = JSON.parse(fs.readFileSync('../config/AlisToken.json', 'utf8'));
 
 module.exports = function deployContracts(deployer) {
-  deployer.deploy(AlisToken, initParams.amount, initParams.name, initParams.decimal, initParams.symbol);
+  const actualInitialAmount = initParams.initialAmount * (10 ** initParams.decimal);
+  const actualOfferedAmount = initParams.offeredAmount * (10 ** initParams.decimal);
+
+  deployer.deploy(AlisToken, actualInitialAmount, initParams.name, initParams.decimal, initParams.symbol,
+    actualOfferedAmount);
 };
