b69f7a3b2725473b67c938dbc328ad119b2469f3
sot528 <sot528@gmail.com>
2017-07-22 23:06:26 +0900

    refactoring: eslint errors.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
+
 const AlisToken = artifacts.require('AlisToken.sol');
 const initParams = JSON.parse(fs.readFileSync('../config/AlisToken.json', 'utf8'));
 
-module.exports = function (deployer) {
+module.exports = function deployContracts(deployer) {
   deployer.deploy(AlisToken, initParams.amount, initParams.name, initParams.decimal, initParams.symbol);
 };
