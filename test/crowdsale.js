acaa49a32556c0d96692e82de88680f5b774b469
sot528 <sot528@gmail.com>
2017-07-25 17:43:44 +0900

    add test to Crowdsale.

--- test/crowdsale.js
+++ test/crowdsale.js
@@ -1,15 +1,26 @@
 /* global it */
+const AlisToken = artifacts.require('AlisToken.sol');
 const Crowdsale = artifacts.require('Crowdsale.sol');
 
-contract('Crowdsale', (accounts) => {
+contract('Crowdsale', () => {
   let crowdSale; // Deployed CrowdSale.
 
   describe('CONTRACT DEPLOYMENT', () => {
-    it('should FIX', () => Crowdsale.deployed().then(
+    it('should has deployed address of AlisToken', () => Crowdsale.deployed().then(
       (instance) => {
         crowdSale = instance;
-        console.log('pending.');
-        return true;
+
+        return crowdSale.token().then(
+          (tokenAddress) => {
+            assert.equal(AlisToken.address, tokenAddress, `wrong token address: ${tokenAddress}`);
+          },
+        );
+      },
+    ));
+
+    it('should has specified address of AlisFund', () => crowdSale.fund().then(
+      (fund) => {
+        assert.equal(fund, '0x0000000000000000000000000000000000000000', `wrong fund address: ${fund}`);
       },
     ));
   });
