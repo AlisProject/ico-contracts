431a073dd681919753aaeb00f378871648e0f4ef
sot528 <sot528@gmail.com>
2017-07-31 18:40:59 +0900

    edit migration script.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,16 +1,18 @@
 const fs = require('fs');
 
 const AlisToken = artifacts.require('AlisToken.sol');
-const Crowdsale = artifacts.require('Crowdsale.sol');
-const tokenParams = JSON.parse(fs.readFileSync('../config/AlisToken.json', 'utf8'));
+const AlisCrowdsale = artifacts.require('AlisCrowdsale.sol');
+// TODO:
+// const tokenParams = JSON.parse(fs.readFileSync('../config/AlisToken.json', 'utf8'));
 const crowdsaleParams = JSON.parse(fs.readFileSync('../config/Crowdsale.json', 'utf8'));
 
 module.exports = function deployContracts(deployer) {
-  const actualInitialAmount = tokenParams.initialAmount * (10 ** tokenParams.decimal);
-  const actualOfferedAmount = crowdsaleParams.offeredAmount * (10 ** tokenParams.decimal);
+  // TODO:
+  // const actualInitialAmount = tokenParams.initialAmount * (10 ** tokenParams.decimal);
+  // const actualOfferedAmount = crowdsaleParams.offeredAmount * (10 ** tokenParams.decimal);
 
-  return deployer.deploy(AlisToken, actualInitialAmount, tokenParams.name, tokenParams.decimal,
-    tokenParams.symbol).then(
-    () => deployer.deploy(Crowdsale, AlisToken.address, crowdsaleParams.fundAddress, actualOfferedAmount,
-      crowdsaleParams.initialRate));
+  deployer.deploy(AlisToken);
+  deployer.link(AlisToken, AlisCrowdsale);
+  // TODO: start & end block.
+  deployer.deploy(AlisCrowdsale, 900000, 1000000, crowdsaleParams.initialRate, crowdsaleParams.fundAddress);
 };
