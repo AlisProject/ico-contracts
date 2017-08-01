52d1456db62e447c20227dd29a497eb55774e2d8
sot528 <sot528@gmail.com>
2017-08-01 12:17:05 +0900

    refactoring.

--- contracts/AlisCrowdsale.sol
+++ contracts/AlisCrowdsale.sol
@@ -14,16 +14,9 @@ contract AlisCrowdsale is Crowdsale {
   function AlisCrowdsale(uint256 start, uint256 end, uint _rate, address _fundAddress)
   Crowdsale(start, end, _rate, _fundAddress)
   {
-    token = createTokenContract();
   }
 
   function createTokenContract() internal returns (MintableToken) {
     return new AlisToken();
-  }
-
-  // send ether to the fund collection wallet
-  // override to create custom fund forwarding mechanisms
-  function forwardFunds() internal {
-    wallet.transfer(msg.value);
   }
 }
