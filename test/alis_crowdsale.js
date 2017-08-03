41a1e78c77bc521643c79bf2d913173de07d32b5
sot528 <sot528@gmail.com>
2017-08-03 22:26:35 +0900

    move token tests to correct place.

--- test/alis_crowdsale.js
+++ test/alis_crowdsale.js
@@ -22,31 +22,6 @@ contract('AlisCrowdsale', ([investor, wallet, purchaser]) => {
   });
 
   describe('initialized correctly', () => {
-    it('should be correct token name', async function () {
-      const expect = 'AlisToken';
-      const actual = await this.token.name();
-      actual.should.be.equal(expect);
-    });
-
-    it('should be correct token symbol', async function () {
-      const expect = 'ALIS';
-      const actual = await this.token.symbol();
-      actual.should.be.equal(expect);
-    });
-
-    it('should be correct token decimals', async function () {
-      const expect = 18;
-      const actual = await this.token.decimals();
-      actual.toNumber().should.be.equal(expect);
-    });
-
-    it('should be same decimals of ether', async function () {
-      const expect = web3.toWei(1, 'ether');
-      const tokenDecimals = await this.token.decimals();
-      const actual = new web3.BigNumber(1 * (10 ** tokenDecimals));
-      actual.toNumber().should.be.bignumber.equal(expect);
-    });
-
     it('should be correct fund address', async function () {
       // FIXME:
       const expect = '0x38924972b953fb27701494f9d80ca3a090f0dc1c';
