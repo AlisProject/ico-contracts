987f290c7c6ca8f4e4131e2ba84c83a6df87fa68
sot528 <sot528@gmail.com>
2017-08-09 07:28:26 +0900

    fix timing problem.

--- test/alis_crowdsale_refundable.js
+++ test/alis_crowdsale_refundable.js
@@ -3,11 +3,15 @@ import advanceToBlock from './helpers/advanceToBlock';
 import EVMThrow from './helpers/EVMThrow';
 
 import {
-  AlisCrowdsale, cap, rate, initialAlisFundBalance, goal, BigNumber,
+  AlisCrowdsale, cap, rate, initialAlisFundBalance, goal, BigNumber, setTimingToBaseTokenRate
 } from './helpers/alis_helper';
 
 contract('AlisCrowdsale', ([owner, wallet, investor, notInvestor]) => {
   const lessThanGoal = ether(goal).minus(ether(100));
+
+  before(async () => {
+    await setTimingToBaseTokenRate();
+  });
 
   beforeEach(async function () {
     this.startBlock = web3.eth.blockNumber + 10;
